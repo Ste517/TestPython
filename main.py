dd1b9bf271cbf50e5738f7b93bbf200f65727435
Ste517 <66008197+Ste517@users.noreply.github.com>
2023-01-13 19:45:22 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+num_sides = input()
+
+num_sides = int(num_sides)
+
+if num_sides is range(3,5):
+    side_length=100
+
+angle = 360/int(num_sides)
+
+s = getscreen()
+
+for i in range(int(num_sides)):
+    forward(side_length)
+    right(angle)
